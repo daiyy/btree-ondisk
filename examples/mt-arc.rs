cbe5b59a35e153cd734c77d59544ada5d1a5faa5
daiyy <daiyiyang@hotmail.com>
2024-07-25 00:51:24 +0000

    add mt arc example skeleton

--- examples/mt-arc.rs
+++ examples/mt-arc.rs
@@ -1,3 +1,14 @@
+mod mt;
+use std::time::Instant;
+
+const DEFAULT_FILE_SIZE: usize = 100 * 1024 * 1024 * 1024;
+
 #[tokio::main]
 async fn main() {
+    let file_size = DEFAULT_FILE_SIZE;
+    let mut file = mt::File::new(file_size);
+
+    tokio::task::spawn(async move {
+        file.build().await;
+    });
 }
