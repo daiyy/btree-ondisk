3a53b67a84eadcc1af156e6f370d15671a0ee57b
daiyy <daiyiyang@hotmail.com>
2025-09-16 13:01:59 +0000

    conditional init local disk node cache / provide adoptive sync/async api

--- src/cache/localdisk.rs
+++ src/cache/localdisk.rs
@@ -11,7 +11,7 @@ use super::NodeTieredCacheStats;
 
 #[derive(Clone)]
 pub struct LocalDiskNodeCache {
-    hybrid: HybridCache<u64, Vec<u8>>,
+    hybrid: Option<HybridCache<u64, Vec<u8>>>,
     stats: NodeTieredCacheStats,
 }
 
@@ -26,7 +26,10 @@ impl<P: Send + Copy + Into<u64>> NodeCache<P> for LocalDiskNodeCache {
     fn push(&self, p: &P, data: &[u8]) {
         let key = (*p).into();
         self.stats.total_push.fetch_add(1, Ordering::SeqCst);
-        self.hybrid.insert_with_properties(key, data.to_vec(),
+        let Some(ref hybrid) = self.hybrid else {
+            return;
+        };
+        hybrid.insert_with_properties(key, data.to_vec(),
             HybridCacheProperties::default()
                 .with_ephemeral(false)
                 .with_location(Location::OnDisk)
@@ -36,7 +39,10 @@ impl<P: Send + Copy + Into<u64>> NodeCache<P> for LocalDiskNodeCache {
     async fn load(&self, p: P, data: &mut [u8]) -> Result<bool> {
         let key = p.into();
         self.stats.total_load.fetch_add(1, Ordering::SeqCst);
-        match self.hybrid.get(&key).await
+        let Some(ref hybrid) = self.hybrid else {
+            return Ok(false);
+        };
+        match hybrid.get(&key).await
                 .map_err(|e| {
                     let err_msg = format!("{}", e);
                     Error::new(ErrorKind::Other, err_msg)
@@ -56,11 +62,17 @@ impl<P: Send + Copy + Into<u64>> NodeCache<P> for LocalDiskNodeCache {
     fn invalid(&self, p: &P) {
         let key = (*p).into();
         self.stats.total_remove.fetch_add(1, Ordering::SeqCst);
-        self.hybrid.remove(&key);
+        let Some(ref hybrid) = self.hybrid else {
+            return;
+        };
+        hybrid.remove(&key);
     }
 
     fn evict(&self) {
-        self.hybrid.memory().evict_all();
+        let Some(ref hybrid) = self.hybrid else {
+            return;
+        };
+        hybrid.memory().evict_all();
     }
 
     fn get_stats(&self) -> NodeTieredCacheStats {
@@ -69,7 +81,14 @@ impl<P: Send + Copy + Into<u64>> NodeCache<P> for LocalDiskNodeCache {
 }
 
 impl LocalDiskNodeCache {
-    pub async fn new(dir: impl AsRef<Path>, capacity: usize, mode: LocalDiskNodeCacheOpenMode) -> Self {
+    pub fn new_none() -> Self {
+        Self {
+            hybrid: None,
+            stats: NodeTieredCacheStats::default(),
+        }
+    }
+
+    pub async fn new_async(dir: impl AsRef<Path>, capacity: usize, mode: LocalDiskNodeCacheOpenMode) -> Self {
         if mode == LocalDiskNodeCacheOpenMode::Recreate {
             let _ = tokio::fs::remove_dir_all(&dir).await;
         }
@@ -95,13 +114,45 @@ impl LocalDiskNodeCache {
         }
 
         Self {
-            hybrid,
+            hybrid: Some(hybrid),
             stats: NodeTieredCacheStats::default(),
         }
     }
 
+    #[maybe_async::maybe_async]
+    pub async fn new(dir: impl AsRef<Path>, capacity: usize, mode: LocalDiskNodeCacheOpenMode) -> Self {
+        #[cfg(not(feature = "sync-api"))]
+        let s = Self::new_async(dir, capacity, mode).await;
+        #[cfg(all(feature = "sync-api", feature = "futures-runtime"))]
+        let s = futures::executor::block_on(async {
+            Self::new_async(dir, capacity, mode).await
+        });
+        #[cfg(all(feature = "sync-api", feature = "tokio-runtime"))]
+        let s = tokio::runtime::Handle::current().block_on(async {
+            Self::new_async(dir, capacity, mode).await
+        });
+        s
+    }
+
+    pub async fn close_async(&self) {
+        let Some(ref hybrid) = self.hybrid else {
+            return;
+        };
+        hybrid.memory().flush().await;
+        hybrid.close().await.expect("failed to close hybrid");
+    }
+
+    #[maybe_async::maybe_async]
     pub async fn close(&self) {
-        self.hybrid.memory().flush().await;
-        self.hybrid.close().await.expect("failed to close hybrid");
+        #[cfg(not(feature = "sync-api"))]
+        self.close_async().await;
+        #[cfg(all(feature = "sync-api", feature = "futures-runtime"))]
+        futures::executor::block_on(async {
+            self.close_async().await;
+        });
+        #[cfg(all(feature = "sync-api", feature = "tokio-runtime"))]
+        tokio::runtime::Handle::current().block_on(async {
+            self.close_async().await;
+        });
     }
 }
